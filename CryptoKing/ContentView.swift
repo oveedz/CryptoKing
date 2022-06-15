ad0d28cee39ba72a2b19bed101fa60a10a498d98
oveedz <ogarcia83@gmail.com>
2022-06-15 14:27:10 -0400

    Initial Commit

---
+++ CryptoKing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CryptoKing
+//
+//  Created by Ovidio  on 6/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoKing/CryptoKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoKingApp.swift
+//  CryptoKing
+//
+//  Created by Ovidio  on 6/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
